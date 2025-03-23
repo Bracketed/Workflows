c8ad591d5741aaf232790fdedf51da73dd302b8d
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2025-03-23 23:24:28 +0000

    Push runCommand() fix

--- src/utils/runCommand.ts
+++ src/utils/runCommand.ts
@@ -2,11 +2,11 @@ import { Logger } from '@bracketed/logger';
 import { execSync } from 'node:child_process';
 const Console = new Logger({ prefix: 'Terminal' });
 
-export function runCommand(command: string): string {
+export function runCommand(command: string): string | undefined {
 	try {
 		Console.info(`Running: ${command}`);
 		const output = execSync(command, { encoding: 'utf8', stdio: 'inherit' });
-		return output.trim();
+		return output ? output.trim() : undefined;
 	} catch (error) {
 		Console.error('Error running command:', error);
 		return '';
