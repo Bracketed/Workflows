3a1b93844b9ed8c5aca4af97e804898f5966c644
ninjaninja140 <edenkneale@outlook.com>
2025-03-24 12:15:17 +0000

    Push fixed entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 node --version
 
+yarn node ./dist/index.mjs
+
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 git commit -a -m "Update README.md from Publish Container - $(git log -1 --pretty=format:"%an") $(date "+%d/+%m/+%Y")"
 git push https://x-access-token:$GH_TOKEN@github.com/$(git remote get-url origin | sed -E 's/.*github\.com[:\/]([^\/]+)\/([^\/]+).*/\1\/\2/') $(git rev-parse --abbrev-ref HEAD)
 
-yarn node ./dist/index.mjs
+exit 0
