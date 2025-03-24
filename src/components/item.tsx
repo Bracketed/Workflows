e93421079b74c334f723eb5a8d4b1d3f09a2e4a2
ninjaninja140 <edenkneale@outlook.com>
2025-03-24 12:52:47 +0000

    Push test update for new labels

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -2,6 +2,7 @@ import { Logger } from '@bracketed/logger';
 import * as cheerio from 'cheerio';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import { getGitBranch } from 'src/utils';
 import TurndownService from 'turndown';
 import type { FinderItem } from '../types';
 
@@ -10,20 +11,27 @@ const Component: React.FC<FinderItem> = (props: FinderItem) => (
 		<h4>
 			<a href={props.content.url}>{props.content.name}</a>
 		</h4>
+		<p>
+			Component link: `{props.file}@{getGitBranch()}`
+		</p>
 		<p>- {props.content.description}</p>
-		<b>Inputs:</b>
-		<ul>
-			{props.content.inputs.map((i, ind) => (
-				<li key={ind}>
-					<b>{i.name}</b>: {i.values.description}
-					<ul>
-						<li>Required: `{i.values.required ? 'true' : 'false'}`</li>
-						{i.values.default ? <li>Default: `{String(i.values.default)}`</li> : undefined}
-						<li>Type: `{i.values.type ?? 'string'}`</li>
-					</ul>
-				</li>
-			))}
-		</ul>
+		{props.content.inputs.length !== 0 ? (
+			<div>
+				<b>Inputs:</b>
+				<ul>
+					{props.content.inputs.map((i, ind) => (
+						<li key={ind}>
+							<b>{i.name}</b>: {i.values.description}
+							<ul>
+								<li>Required: `{i.values.required ? 'true' : 'false'}`</li>
+								{i.values.default ? <li>Default: `{String(i.values.default)}`</li> : undefined}
+								<li>Type: `{i.values.type ?? 'string'}`</li>
+							</ul>
+						</li>
+					))}
+				</ul>
+			</div>
+		) : undefined}
 	</div>
 );
 
